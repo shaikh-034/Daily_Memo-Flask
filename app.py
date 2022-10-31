5f66c444a6a064670c01293b4ee04dbc369991bb
shaikh-034 <shaikharfan034@gmail.com>
2022-10-31 17:21:55 +0530

    view

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
+@app.route("/a")
+def create():
+    return "<p>Hello, !</p>"
+
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
